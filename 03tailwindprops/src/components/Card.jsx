eb1b59ca212431c7c66dab1a9ebf3b1a7b450552
Yash Karande <yashkarande4240@gmail.com>
2026-01-03 20:48:34 +0530

    tailwindprops project done

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Card({brand = "default value if props not send"}) {
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm border border-white-400 p-4 rounded-lg">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{brand}</h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
